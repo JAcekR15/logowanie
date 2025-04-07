25dd62df8e53ae91f405dbead20ebcd9f69d8d3c
JAcekR15 <jack.robakiewicz@gmail.com>
2025-04-07 11:05:50 +0200

    final 07.04.2025

--- client/index.js
+++ client/index.js
@@ -13,3 +13,24 @@ async function login() {
     console.log(data);
 
 }
+
+
+async function register() {
+    const login = document.getElementById("login1").value;
+    const password = document.getElementById("password1").value; 
+    const password2 = document.getElementById("password2").value; 
+
+    if (password !== password2){
+        console.log("Hasła są różne");
+    }
+    else{
+        const baseURL = `http://localhost:3000`
+
+        const url = `${baseURL}/register/${login}/${password}`;
+        const data = await fetch(url);
+        const response = await data.json();
+        console.log(response);
+        console.log(data);
+    }
+
+}
--- server/server.js
+++ server/server.js
@@ -1,47 +1,64 @@
-const express = require('express');
-const cors = require('cors');
-const mysql = require('mysql');
+const express = require("express");
+const cors = require("cors");
+const mysql = require("mysql");
 const app = express();
 app.use(cors());
 
-
 var conn = mysql.createConnection({
-    host:"localhost",
-    user:"root",
-    password:"",
-    database:"2progr2_users"
- })
- conn.connect((err)=>{
-    if(err){
-        console.log("Błąd w połączeniu z bazą");
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "2progr2_users",
+});
+conn.connect((err) => {
+  if (err) {
+    console.log("Błąd w połączeniu z bazą");
+  } else {
+    console.log("Połączenie udane");
+  }
+});
+app.get("/login/:login/:password", (req, res) => {
+  let login = req.params.login;
+  let password = req.params.password;
+
+  console.log(login);
+  console.log(password);
+  const sql = `SELECT * FROM users WHERE login='${login}' AND password='${password}'`;
+
+  conn.query(sql, (err, result) => {
+    if (err) throw err;
+    console.log(result);
+    if (result.length > 0) {
+      res.json({ status: "ok" });
+    } else {
+      res.json({ status: "error" });
     }
-    else {
-        console.log("Połączenie udane");
+  });
+});
+app.get("/register/:login/:password", (req, res) => {
+  let login = req.params.login;
+  let password = req.params.password;
+
+  const sql = `INSERT INTO users(login,password,uprawnienia) VALUES ('${login}','${password}','user')`;
+  const sql2 = `SELECT login FROM users WHERE login = '${login}'`;
+
+  conn.query(sql2, (err, result) => {
+    console.log(result);
+    if (result.length > 0) {
+      res.status(409).json({ message: "login jest zajęty" });
+    } else {
+      conn.query(sql, (err, result) => {
+        if (err) throw err;
+        if (result.affectedRows > 0) {
+          res.json({ status: "ok" });
+        } else {
+          res.json({ status: "error" });
+        }
+      });
     }
- })
-app.get('/login/:login/:password',(req,res)=>{
- let login = req.params.login;
- let password = req.params.password;
- 
- console.log(login);
- console.log(password);
- const sql = `INSERT INTO users(login, password, uprawnienia) VALUES ('${login}','${password}','admin')`;
-    
- conn.query(sql, (err, dane, info) => {
-    if(err){
-        console.log("Nie udało się wstawic rekordu");
+  });
+});
 
-    }
-    else{
-        console.log("wstawiono rekord");
-    }
- })
-
-})
-
-
-
-
-app.listen(3000,()=>{
-    console.log("ok");
-})
+app.listen(3000, () => {
+  console.log("ok");
+});
